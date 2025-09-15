96a2595c523e109730b57ca8bb19b640cb827d1a
Dragan Peric <draganceperic@gmail.com>
2025-09-15 20:01:22 +0200

    Added MicroJava runtime library to the project

---
+++ src/rs/ac/bg/etf/pp1/CodeGenerator.java
@@ -0,0 +1,13 @@
+package rs.ac.bg.etf.pp1;
+
+import rs.ac.bg.etf.pp1.ast.*;
+import rs.etf.pp1.symboltable.*;
+import rs.etf.pp1.symboltable.concepts.*;
+import rs.etf.pp1.mj.runtime.Code;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class CodeGenerator extends VisitorAdaptor {
+	
+}
